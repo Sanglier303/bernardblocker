db41f918c5ead9c0d5592adb923171949487dcaf
Sanglier303 <pixngeek+sanglier@gmail.com>
2026-09-21 13:06:44 +0200

    Add Bernard device-admin receiver for Fortress mode

---
+++ app/src/main/java/com/local/focusfence/security/BernardDeviceAdminReceiver.java
@@ -0,0 +1,7 @@
+package com.local.focusfence.security;
+
+import android.app.admin.DeviceAdminReceiver;
+
+/** Device-admin component used only when Bernard is provisioned as Android Device Owner. */
+public final class BernardDeviceAdminReceiver extends DeviceAdminReceiver {
+}
