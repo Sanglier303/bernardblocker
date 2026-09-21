9771b73020d3028dd6010276042050c4e8f05c1a
Sanglier303 <pixngeek+sanglier@gmail.com>
2026-09-21 18:36:28 +0200

    Do not hijack ordinary System UI events during enforcement

--- app/src/main/java/com/local/focusfence/security/TamperGuard.java
+++ app/src/main/java/com/local/focusfence/security/TamperGuard.java
@@ -51,8 +51,7 @@ public final class TamperGuard {
                 || "com.samsung.android.sm".equals(pkg)
                 || "com.miui.powerkeeper".equals(pkg)
                 || "com.coloros.phonemanager".equals(pkg)
-                || "com.oneplus.security".equals(pkg)
-                || "com.android.systemui".equals(pkg);
+                || "com.oneplus.security".equals(pkg);
     }
 
     /**
